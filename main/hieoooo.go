f6c7498929a0faa9d788698cadcc6fe7fe3f6ead
RaviTejaPottabatini <52947830+RaviTejaPottabatini@users.noreply.github.com>
2019-08-05 19:22:53 +0530

    Update hieoooo.go

--- main/hieoooo.go
+++ main/hieoooo.go
@@ -1,153 +1,29 @@
-package main 
-
+package main
 
 import (
-
-    "fmt"
+  "fmt"
+  "io"
   "log"
-  //"encoding/json"
-  //"io/ioutil"
-  //"io"
   "os"
-  "github.com/fsnotify/fsnotify"
-
+  "io/ioutil"
+  "path/filepath"
 )
 
-/*
-
-type Config struct{
-        Model string `json:"model"`
-        Src string `json:"src"`
-        Destination string `json : "destination"`
-        Support string `json : "support"`
-
-      
-    } 
-
-
-/*func LoadConfiguration(file string) Config {
-    var config Config
-    configFile, err := os.Open(file)
-    defer configFile.Close()
-    if err != nil {
-        fmt.Println(err.Error())
-    }
-    jsonParser := json.NewDecoder(configFile)
-    jsonParser.Decode(&config)
-    return config
-}*/
-
-/*
-
-func jsonconfig(file string) Config{
-
-  var jsondata Config
-  //json_file := ("link of the file ")
-
-  file, err := ioutil.ReadFile(filename)
-   if err != nil {
-        fmt.Printf("%s", err)
-        os.Exit(0)
-    }
-
-    defer file.Body.Close()
-
-    filejson ,err  :=ioutil.ReadAll(file.Body)
-        if err != nil {
-            fmt.Printf("%s", err)
-            os.Exit(0)
-        } 
-
-
-    //json.Unmarshal([]byte(realdata), &data)
-    json.Unmarshal([]byte(filejson), &jsondata)
-    fmt.Println(jsondata)
-    return jsondata
-}*/
-
-func CopyFile(src, dst string) (err error) {
-    sfi, err := os.Stat(src)
-    if err != nil {
-        return
-    }
-    if !sfi.Mode().IsRegular() {
-        // cannot copy non-regular files (e.g., directories,
-        // symlinks, devices, etc.)
-        return fmt.Errorf("CopyFile: non-regular source file %s (%q)", sfi.Name(), sfi.Mode().String())
-    }
-    dfi, err := os.Stat(dst)
-    if err != nil {
-        if !os.IsNotExist(err) {
-            return
-        }
-    } else {
-        if !(dfi.Mode().IsRegular()) {
-            return fmt.Errorf("CopyFile: non-regular destination file %s (%q)", dfi.Name(), dfi.Mode().String())
-        }
-        if os.SameFile(sfi, dfi) {
-            return
-        }
-    }
-    if err = os.Link(src, dst); err == nil {
-        return
-    }
-   // err = copyFileContents(src, dst)
-    return
-}
-
-
+var target_path string
+var source_path string
 
 func main() {
 
-  var dst string
-  var src string
+  source_path = "D:/rapiscan/input/632DV"
+  target_path = "D:/alredyexists"
 
-  
+  err := CopyDir(source_path, target_path)
+  if err != nil {
+    log.Fatal(err)
+  } else {
+    fmt.Print("copy finish")
+  }
 
-    // json_data , _ := jsonconfig("vndgh.json")
-
-
-            
-      watcher, err := fsnotify.NewWatcher()
-      if err != nil {
-          log.Fatal(err)
-      }
-
-       err = watcher.Add("D:\\rapiscan\\input\\632DV")
-      if err != nil {
-          log.Fatal(err)
-        }
-
-//mailto:rashi.sweet8810@yahoo.com
-
-      defer watcher.Close()
-
-      //done := make(chan bool)
-       func() {
-          for {
-              select {
-              case Event := <-watcher.Events:
-                  fmt.Println("event: in", Event)
-                  if Event.Op&fsnotify.Write == fsnotify.Write {
-                      fmt.Println("modified file:", Event.Name)
-                      
-                        src = Event.Name
-                        copy(src , dst)
-                        fmt.Println("file is copied")
-                      
-                  }
-              case err := <-watcher.Errors:
-                  fmt.Println("error:", err)
-              }
-          }
-      }()    
-     
-      //<-done
- 
-
-
-
- 
 }
 
 
@@ -160,6 +36,99 @@ func main() {
 
 
 
+func CopyFile(src, dst string) (err error) {
+  in, err := os.Open(src)
+  if err != nil {
+    return
+  }
+  defer in.Close()
 
+  out, err := os.Create(dst)
+  if err != nil {
+    return
+  }
+  defer func() {
+    if e := out.Close(); e != nil {
+      err = e
+    }
+  }()
 
+  _, err = io.Copy(out, in)
+  if err != nil {
+    return
+  }
 
+  err = out.Sync()
+  if err != nil {
+    return
+  }
+
+  si, err := os.Stat(src)
+  if err != nil {
+    return
+  }
+  err = os.Chmod(dst, si.Mode())
+  if err != nil {
+    return
+  }
+
+  return
+}
+
+// CopyDir recursively copies a directory tree, attempting to preserve permissions.
+// Source directory must exist, destination directory must *not* exist.
+// Symlinks are ignored and skipped.
+func CopyDir(src string, dst string) (err error) {
+  src = filepath.Clean(src)
+  dst = filepath.Clean(dst)
+
+  si, err := os.Stat(src)
+  if err != nil {
+    return err
+  }
+  if !si.IsDir() {
+    return fmt.Errorf("source is not a directory")
+  }
+
+  _, err = os.Stat(dst)
+  if err != nil && !os.IsNotExist(err) {
+    return
+  }
+  if err == nil {
+    return fmt.Errorf("destination already exists")
+  }
+
+  err = os.MkdirAll(dst, si.Mode())
+  if err != nil {
+    return
+  }
+
+  entries, err := ioutil.ReadDir(src)
+  if err != nil {
+    return
+  }
+
+  for _, entry := range entries {
+    srcPath := filepath.Join(src, entry.Name())
+    dstPath := filepath.Join(dst, entry.Name())
+
+    if entry.IsDir() {
+      err = CopyDir(srcPath, dstPath)
+      if err != nil {
+        return
+      }
+    } else {
+      // Skip symlinks.
+      if entry.Mode()&os.ModeSymlink != 0 {
+        continue
+      }
+
+      err = CopyFile(srcPath, dstPath)
+      if err != nil {
+        return
+      }
+    }
+  }
+
+  return
+}
